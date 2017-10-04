03c61aebabaace889c5ea38cea501ffecdbba9a7
Wojciech Stokowiec <wstokowiec@Wojciechs-MacBook-Pro.local>
2017-10-04 11:37:20 -0400

    added installing python to mac setup sctipt

--- setup-mac.sh
+++ setup-mac.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+
 #install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 #instal core utils to replace ls
-brew isntall vim
+brew install vim
 brew install nvim
 brew install coreutils
+brew install python3
+
+# install ipython
+pip3 install ipython
